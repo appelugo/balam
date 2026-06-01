378b650ccb7dfe98619b9616817773a0d9265cf4
appelugo <app.elugo@gmail.com>
2026-06-01 15:56:11 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDtlEE7cmyjWvppnO9JDu1A3OsNxpCr5Nc",
+    authDomain: "balam-4e843.firebaseapp.com",
+    projectId: "balam-4e843",
+    storageBucket: "balam-4e843.firebasestorage.app",
+    messagingSenderId: "633718462390",
+    appId: "1:633718462390:web:aaefbe4bf1cc3917fc5317"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
